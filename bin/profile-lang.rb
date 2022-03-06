4d64f5c24115ae7b76f321e389c24c8149de2ab0
David Vargas <dvarrui@protonmail.com>
2022-03-06 07:46:37 +0000

    [U] .

--- bin/profile-lang.rb
+++ bin/profile-lang.rb
@@ -1,11 +1,28 @@
 #!/usr/bin/env ruby
 
-def get_features
-  dirpath = File.join('..', 'features')
-  features = Dir[ "#{dirpath}/*.d" ]
+class Profiler
+  attr_reader :features
+
+  def initialize
+    @features = read_features || []
+  end
+
+  def features_dirbase
+    dirbase = File.dirname(__FILE__)
+    File.join(dirbase, '..', 'features')
+  end
+
+  private
+
+  def read_features
+    output = []
+    filepaths = Dir[ "#{features_dirbase}/*.d" ] 
+    filepaths.sort.each { |filepath| output <<  File.basename(filepath) }
+    output
+  end
 end
 
-features = get_features
+profiler = Profiler.new
+puts profiler.features
 
-puts features
 
